7ab07e09c59bf70a317daf74a961af615cbafd2e
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-04-03 15:55:19 +0100

    wip In vers.sh env.local updating version finding scripts

--- scripts/vers.sh
+++ scripts/vers.sh
@@ -2,5 +2,5 @@
 echo VERSION NUMBERS:
 echo "   npm published: `npm view $NPMPKG version`"
 echo "   local: `packagever.sh`"
-egrep 'version.*[:=].*[0-9]+\.[0-9]+\.[0-9]' node_modules/mocha/package.json $VERFILES \
+egrep 'version.*[:=].*[0-9]+\.[0-9]+\.[0-9]' node_modules/istanbul-reports/package.json $VERFILES \
 | perl -pne '$_ = qq{   $_}'
