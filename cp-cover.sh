f6b4ce30a16f401f2042d457a7b7143cb7fa41a7
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-04-07 08:04:27 +0100

    wip Improving syntax highlight colors for better dark contrast
    also fixed cp-cover.sh command.

--- cp-cover.sh
+++ cp-cover.sh
@@ -3,8 +3,9 @@ DIR=${1:-coverage}
 CHANGED=1
 FROM=./node_modules/nyc-dark/
 
-while /bin/true; do
-	if [ $CHANGED ]; then
+while [ 1 == 1 ]; do
+	if [ $CHANGED != 0 ]; then
+		echo `date` copy dark stylesheet from $FROM/ to $DIR/
 		cp $FROM/*.css $DIR/
 	fi
 	sleep 1
